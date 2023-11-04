25510fc29718fd2477d504eb473034ca015db7d9
VoxelMC <voxelmc2@student.ubc.ca>
2023-11-03 19:01:26 -0700

    Create Parts Page and Make SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
   outDir: "public",
   publicDir: "static",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
