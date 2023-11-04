2063658483f60d9f811100a9c7f26e21cf57f32f
SatanshuMishra <satanshumishra@outlook.com>
2023-11-04 02:20:49 -0700

    🏗️Completed Promoters - Completed Promoters page - Added Navigation - Started work on mega navigation bar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
   outDir: "public",
   publicDir: "static",
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
